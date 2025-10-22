0440220ee66c80ee0a0b16f560c9a7142732f32e
Chan Xian Kang <18016386@imail.sunway.edu.my>
2025-10-22 22:29:21 +0100

    firebase4

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -4,12 +4,13 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-auth
 import { getFirestore } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyXXXXXX",
-  authDomain: "auth-demo.firebaseapp.com",
-  projectId: "auth-demo",
-  storageBucket: "auth-demo.appspot.com",
-  messagingSenderId: "1234567890",
-  appId: "1:1234567890:web:abcdef123456",
+  apiKey: "AIzaSyBhTw86GVGlsXmYkvWEZmtrrXKOWMNLe8s",
+  authDomain: "database-intern.firebaseapp.com",
+  projectId: "database-intern",
+  storageBucket: "database-intern.appspot.com",
+  messagingSenderId: "908483294915",
+  appId: "1:908483294915:web:a48bb7c3813390cf709476",
+  measurementId: "G-T93PLPMN4L"
 };
 
 export const app = initializeApp(firebaseConfig);
